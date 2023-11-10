336d8e7dd94451fca976a0ef92943d910d21adf0
Егор <124521323+f1k13@users.noreply.github.com>
2023-11-10 11:50:44 +0300

    feat: add path to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/test-vue-consalting",
   plugins: [vue()],
   resolve: {
     alias: [
